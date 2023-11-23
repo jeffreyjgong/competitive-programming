7a7f14edacac42399499996450939e0593828b74
Jeffrey Gong <jeffrey.gong@yale.edu>
2023-11-23 11:53:09 -0500

    lc 1379

--- test.cpp
+++ test.cpp
@@ -3,20 +3,5 @@
 using namespace std;
 
 int main() {
-    map<int, string> mymap;
-
-    if (mymap.begin() == mymap.end()) {
-        cout << "yay same" << endl;
-    }
-
-    mymap[1] = 2;
-
-    auto it = mymap.lower_bound(4);
     
-    
-    if (it == mymap.begin()) {
-        cout << "begin" << endl;
-    } else if (it == mymap.end()) {
-        cout << "end" << endl;
-    }
 }
