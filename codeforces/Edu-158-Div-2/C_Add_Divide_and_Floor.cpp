31dcf84b6d35c9b13f37fc63accc388c8f6766a9
Jeffrey Gong <jeffrey.gong@yale.edu>
2023-11-24 12:04:31 -0500

    Edu-158-Div-2: C

---
+++ codeforces/Edu-158-Div-2/C_Add_Divide_and_Floor.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+
+using ll = long long;
+using ld = long double;
+using namespace std;
+#define endl "\n";
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void print(T outVar) {cout << outVar << endl;}
+template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << endl; print(args ...);}
+#define ff first
+#define D(x) cerr << #x " is " << (x) << endl
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << endl
+#define foreach(x, v) for (__typeof__ (v).begin() x=(v).begin(); x != (v).end(); ++x)
+#define For(i, a, b) for (int i=(a); i<(b); ++i)
+#define DFor(i, a, b) for (int i=(a); i>=b; --i)
+#define ss second
+#define int ll
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+void solve(){
+    int n;
+    input(n);
+
+    int MAX = INT_MIN;
+    int MIN = INT_MAX;
+
+    for(int i = 0; i<n; i++) {
+        int x;
+        input(x);
+
+        MAX = max(MAX, x);
+        MIN = min(MIN, x);
+    }
+
+    vector<int> res;
+
+    while(MIN != MAX) {
+        if (MAX & 1) {
+            // odd
+            res.emplace_back(0);
+            MAX /= 2;
+            MIN /= 2;
+        } else {
+            // even
+            res.emplace_back(1);
+            MAX = (MAX + 1) / 2;
+            MIN = (MIN + 1) / 2;
+        }
+    }
+
+    const int m = res.size();
+    cout << m << endl;
+    if (m <= n) {
+        For(i, 0, m) {
+            cout << res[i] << " ";
+        }
+        cout << endl;
+    }
+
+}
+
+int32_t main() {
+    setup();
+
+    int T = 1;
+    cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
