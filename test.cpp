de4e1514658b8f1050c3aa8b6e8e577828ba339b
Jeffrey Gong <jeffrey.gong@yale.edu>
2023-11-26 12:08:57 -0500

    add ordered_set to macros

--- test.cpp
+++ test.cpp
@@ -1,9 +1,34 @@
 #include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
 
+using ll = long long;
+using ld = long double;
+using namespace __gnu_pbds;
 using namespace std;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define endl "\n";
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void print(T outVar) {cout << outVar << endl;}
+template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << endl; print(args ...);}
+#define ff first
+#define D(x) cerr << #x " is " << (x) << endl
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << endl
+#define foreach(x, v) for (__typeof__ (v).begin() x=(v).begin(); x != (v).end(); ++x)
+#define For(i, a, b) for (int i=(a); i<(b); ++i)
+#define DFor(i, a, b) for (int i=(a); i>=b; --i)
+#define ss second
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 
-template <typename T>
-using ordered_set = tree<
 int main() {
-
+    ordered_set<int> s;
+    s.insert(1);
+    s.insert(3);
+    s.insert(9);
+    cout << s.order_of_key(2) << endl; // index of 2: number of elements in s less than 2
+    cout << *s.find_by_order(0) << endl; // print the 0-th smallest number in s (0-based)
+    cout << *s.find_by_order(2) << endl;
 }
