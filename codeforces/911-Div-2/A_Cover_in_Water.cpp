3cd3105a4fa1e9269f58342818e5e433936a92b8
Jeffrey Gong <jeffrey.gong@yale.edu>
2023-11-26 11:43:22 -0500

    911-Div-2: A

---
+++ codeforces/911-Div-2/A_Cover_in_Water.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+
+using ll = long long;
+using ld = long double;
+using namespace std;
+#define endl "\n";
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void print(T outVar) {cout << outVar << endl;}
+template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << endl; print(args ...);}
+#define ff first
+#define D(x) cerr << #x " is " << (x) << endl
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << endl
+#define foreach(x, v) for (__typeof__ (v).begin() x=(v).begin(); x != (v).end(); ++x)
+#define For(i, a, b) for (int i=(a); i<(b); ++i)
+#define DFor(i, a, b) for (int i=(a); i>=b; --i)
+#define ss second
+#define int ll
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+void solve(){
+    int n;
+    input(n);
+
+    string s;
+    input(s);
+
+    int cur_empty = 0;
+    int total_empty = 0;
+
+    for(int i = 0; i<n; i++) {
+        if (s[i] == '.') {
+            total_empty++;
+            cur_empty++;
+            if (cur_empty == 3) {
+                cout << 2 << endl;
+                return;
+            }
+        } else {
+            cur_empty = 0;
+        }
+    }
+
+    cout << total_empty << endl;
+}
+
+int32_t main() {
+    setup();
+
+    int T = 1;
+    cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+template <typename T>
+using ordered_set = tree<
 int main() {
-    
+
 }
