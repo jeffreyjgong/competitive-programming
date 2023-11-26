06c7e37a4f1b6ac0cc6e42b520fbdeb17f1acee4
Jeffrey Gong <jeffrey.gong@yale.edu>
2023-11-26 11:43:49 -0500

    911-Div-2: unfinished

---
+++ codeforces/911-Div-2/D_Small_GCD.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+
+using ll = long long;
+using ld = long double;
+using namespace std;
+#define endl "\n";
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void print(T outVar) {cout << outVar << endl;}
+template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << endl; print(args ...);}
+#define ff first
+#define D(x) cerr << #x " is " << (x) << endl
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << endl
+#define foreach(x, v) for (__typeof__ (v).begin() x=(v).begin(); x != (v).end(); ++x)
+#define For(i, a, b) for (int i=(a); i<(b); ++i)
+#define DFor(i, a, b) for (int i=(a); i>=b; --i)
+#define ss second
+#define int ll
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+void solve(){
+    int n;
+    input(n);
+
+    vector<int> a(n);
+
+    arrPut(a);
+
+}
+
+int32_t main() {
+    setup();
+
+    int T = 1;
+    cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
