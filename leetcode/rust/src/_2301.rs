4e3693f90a9b07bfb6d2b322bfe275fb36c9b13a
Jeffrey Gong <jeffrey.gong@yale.edu>
2023-12-13 17:52:25 -0500

    initial rust lc setup

---
+++ leetcode/rust/src/_2301.rs
@@ -0,0 +1,24 @@
+#![allow(unused_imports)]
+
+use std::collections::{
+    VecDeque, LinkedList,
+    HashMap, BTreeMap,
+    HashSet, BTreeSet,
+    BinaryHeap
+};
+
+use core::cmp::{
+    Eq, PartialEq,
+    Ord, PartialOrd,
+    Ordering,
+    Reverse,
+    max, min
+};
+
+struct Solution;
+
+impl Solution {
+    pub fn match_replacement(s: String, sub: String, mappings: Vec<Vec<char>>) -> bool {
+        
+    }
+}
---
+++ leetcode/rust/src/main.rs
@@ -0,0 +1,22 @@
+#![allow(unused_imports)]
+
+pub mod _2301;
+
+use std::collections::{
+    VecDeque, LinkedList,
+    HashMap, BTreeMap,
+    HashSet, BTreeSet, 
+    BinaryHeap
+};
+
+use core::cmp::{
+    Eq, PartialEq,
+    Ord, PartialOrd,
+    Ordering, 
+    Reverse,
+    max, min
+};
+
+fn main() {
+    println!("This is my Rust template for leetcode.");
+}
