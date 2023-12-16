550a0a347d6a6fa382e03b36332715a18ec9dd58
Jeffrey Gong <jeffrey.gong@yale.edu>
2023-12-16 12:17:30 -0500

    915-Div-2: B

---
+++ codeforces/915-Div-2/B_Begginer_s_Zelda.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<numeric>
+
+using ll = long long;
+using ld = long double;
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define endl "\n";
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void print(T outVar) {cout << outVar << endl;}
+template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << endl; print(args ...);}
+#define ff first
+#define D(x) cerr << #x " is " << (x) << endl
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << endl
+#define foreach(x, v) for (__typeof__ (v).begin() x=(v).begin(); x != (v).end(); ++x)
+#define For(i, a, b) for (int i=(a); i<(b); ++i)
+#define DFor(i, a, b) for (int i=(a); i>=b; --i)
+#define ss second
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define int ll
+
+void solve(){
+    int n;
+    input(n);
+
+    vector<int> edges(n+1, 0);
+
+    int u, v;
+    for(int i = 0; i<n-1; i++) {
+        cin >> u >> v;
+        edges[u]++;
+        edges[v]++;
+    }
+
+    int total_leaves = 0;
+    for(int i = 1; i<=n; i++) {
+        if (edges[i] == 1) {
+            total_leaves++;
+        }
+    }
+
+    cout << ((total_leaves + 1) / 2) << endl;
+}
+
+int32_t main() {
+    setup();
+
+    int T = 1;
+    cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
