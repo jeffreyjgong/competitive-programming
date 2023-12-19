fbbb774babb234e4600fecc5c18416ef41b2ac38
Jeffrey Gong <jeffrey.gong@yale.edu>
2023-12-19 12:28:53 -0500

    916-Div-3: B

---
+++ codeforces/916-Div-3/B_Preparing_for_the_Contest.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<numeric>
+
+using ll = long long;
+using ld = long double;
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define endl "\n";
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void print(T outVar) {cout << outVar << endl;}
+template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << endl; print(args ...);}
+#define ff first
+#define D(x) cerr << #x " is " << (x) << endl
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << endl
+#define foreach(x, v) for (__typeof__ (v).begin() x=(v).begin(); x != (v).end(); ++x)
+#define For(i, a, b) for (int i=(a); i<(b); ++i)
+#define DFor(i, a, b) for (int i=(a); i>=b; --i)
+#define ss second
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define int ll
+
+void solve(){
+    int n, k;
+    input(n, k);
+
+    int start = n - k;
+    vector<int> res;
+    for(int i = start; i<=n; i++) {
+        res.push_back(i);
+    }
+
+    for(int i = start-1; i>=1; i--) {
+        res.push_back(i);
+    }
+
+    for(auto i : res) {
+        cout << i << " ";
+    }
+
+    cout << endl;
+}
+
+int32_t main() {
+    setup();
+
+    int T = 1;
+    cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
