07675343fe8e130e306625d51cf0b07bcc109950
Jeffrey Gong <jeffrey.gong@yale.edu>
2023-12-20 19:36:23 -0500

    Edu-160-Div-2-Virtual: C

---
+++ codeforces/Edu-160-Div-2-Virtual/C_Game_with_Multiset.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<numeric>
+
+using ll = long long;
+using ld = long double;
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define endl "\n";
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void println(T outVar) {cout << outVar << endl;}
+template<typename T, typename... S> inline void println(T outVar, S... args) {cout << outVar << endl; print(args ...);}
+#define ff first
+#define D(x) cerr << #x " is " << (x) << endl
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << endl
+#define foreach(x, v) for (__typeof__ (v).begin() x=(v).begin(); x != (v).end(); ++x)
+#define For(i, a, b) for (int i=(a); i<(b); ++i)
+#define DFor(i, a, b) for (int i=(a); i>=b; --i)
+#define ss second
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define int ll
+typedef pair<int, int> pii;
+
+int logstore[30];
+
+void solve(){
+    int op, val;
+    input(op, val);
+
+    if (op == 1) {
+        logstore[val]++;
+        return;
+    } else {
+        DFor(i, 29, 0) {
+            int take = min(logstore[i], val >> i);
+            val -= take << i;
+        }
+
+        println((val > 0 ? "NO" : "YES"));
+    }
+}
+
+int32_t main() {
+    setup();
+
+    int T = 1;
+    cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
