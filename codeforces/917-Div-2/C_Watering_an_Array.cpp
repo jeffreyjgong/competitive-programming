3c9033de75effd9e15f32b0f6f905a439c5c2c66
Jeffrey Gong <jeffrey.gong@yale.edu>
2023-12-24 12:44:41 -0500

    917-Div-2: C (practice)

--- codeforces/917-Div-2/C_Watering_an_Array.cpp
+++ codeforces/917-Div-2/C_Watering_an_Array.cpp
@@ -26,79 +26,60 @@ template<typename T, typename... S> inline void println(T outVar, S... args) {co
 #define int ll
 typedef pair<int, int> pii;
 
-void solve(){
+// cur_day is the day that you would do the second operation for the first time
+int cur_score(const vector<int>& a, int n, int total_days, int cur_day) {
+    int sum = 0;
+    For(i, 1, n+1) {
+        if (a[i] == i) {
+            sum++;
+        }
+    }
+
+    return sum + (total_days - cur_day)/2;
+}
+
+void solve() {
     int n, k, d;
     input(n,k,d);
 
-    vector<int> a(n);
-    arrPut(a);
-
-    vector<int> b(k);
-    arrPut(b);
-
-    const int d_mod = k+1;
-    int score = 0;
-
-    unordered_map<int, int> away;
-
-    For(i, 0, n) {
-        // index i is place i+1
-        away[abs((i+1) - a[i])]++;
+    vector<int> a(n+1);
+    For(i, 1, n+1) {
+        int a_elem;
+        input(a_elem);
+        a[i] = a_elem;
     }
 
-    int32_t num_can_add[2001] = {0};
+    vector<int> b(k+1);
+    For(i, 1, k+1) {
+        int b_elem;
+        input(b_elem);
+        b[i] = b_elem;
+    }
 
-    num_can_add[1] = k;
-    For(i, 2, n+1) {
-        For(j, 0, k) {
-            if (b[j] >= i) {
-                num_can_add[i]++;
-            }
+    // EV of a move is always 0.5 because you can always turn 1st one and then immediately cash out
+
+    // we need to go through and check for each day at the start, what the cash out value would be
+    // however, d can be large so we need to somehow restrict what we need to check
+    // well, we know that at most, we can get n score from the array. This is only worth doing if it takes
+    // at most 2n-1 days, because the EV is already n/2 for each day. Thus, we need only check to min(d, 2n-1)
+
+    int max_score = cur_score(a, n, d, 1);
+
+    // i represents the day that you do the second operation
+
+    for(int i = 2; i<=min(d, 2*n); i++) {
+        // need to add day 1s
+        // day 1 -> index 1, day 2 -> index 2, day 3 -> 3
+        // 4 -> 1, 5 -> 2, 6-> 3
+        int range_to_add = b[((i-1) % k) == 0 ? k : (i-1)%k];
+        for(int j = 1; j<=range_to_add; j++) {
+            a[j]++;
         }
+
+        max_score = max(max_score, cur_score(a, n, d, i));
     }
 
-    if (d == 1) {
-        cout << away[0] << endl;
-        return;
-    }
-
-    unordered_map<int, int> days_away;
-    days_away[abs(1 - a[0])] = abs(1 - a[0]);
-
-    For(i, 1, n) {
-        // i+1 is the real place
-        int iterations = abs(i+1 - a[i]) / (num_can_add[i+1]);
-        int remainder = abs(i+1 - a[i]) % num_can_add[i+1];
-
-        if (iterations == 0 && remainder == 0) {
-            days_away[0]++;
-        } else if (remainder == 0) {
-            int startstuff = k*(iterations-1);
-            for(int j = k-1; j>=0; j--) {
-                if (b[j] >= i+1) {
-                    startstuff += (j+1);
-                    break;
-                }
-            }
-            days_away[startstuff]++;
-        } else {
-            int startstuff = k*iterations;
-            int sofar = 0;
-            for(int j = 0; j<k; j++) {
-                if (b[j] >= i+1) {
-                    sofar++;
-                    if (sofar == remainder) {
-                        startstuff+=(j+1);
-                        break;
-                    }
-                }
-            }
-
-            days_away[startstuff]++;
-        }
-    }
-
-    // bruh this is stupid as fuck why am i doing this
+    cout << max_score << endl;
 }
 
 int32_t main() {
