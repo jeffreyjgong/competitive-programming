ffa2af7fbcb35715292cff38d922b90aa057d3c9
Jeffrey Gong <jeffrey.gong@yale.edu>
2023-12-30 20:35:07 -0500

    cses: repetitions

---
+++ cses/intro-problems/repetitions.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<numeric>
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define endl "\n";
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void println(T outVar) {cout << outVar << endl;}
+template<typename T, typename... S> inline void println(T outVar, S... args) {cout << outVar << endl; print(args ...);}
+#define ff first
+#define D(x) cerr << #x " is " << (x) << endl
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << endl
+#define foreach(x, v) for (__typeof__ (v).begin() x=(v).begin(); x != (v).end(); ++x)
+#define For(i, a, b) for (int i=(a); i<(b); ++i)
+#define DFor(i, a, b) for (int i=(a); i>=b; --i)
+#define ss second
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define int ll
+#define uint ull
+#define odd(x) (x & 1)
+typedef pair<int, int> pii;
+
+int32_t main() {
+    setup();
+
+    string s;
+    input(s);
+
+    uint cur = 1;
+    uint maximum = 1;
+
+    For(i, 1, s.size()) {
+        if (s[i] == s[i-1]) {
+            cur++;
+            maximum = max(maximum, cur);
+        } else {
+            cur = 1;
+        }
+    }
+
+    cout << maximum << endl;
+    return 0;
+}
