e3d368e8266fa82077aa40b22c4db9aa00362d7a
Jeffrey Gong <jeffrey.gong@yale.edu>
2024-01-06 16:48:07 -0500

    cses: apple division

--- cses/intro-problems/apple-division.cpp
+++ cses/intro-problems/apple-division.cpp
@@ -30,6 +30,16 @@ template<typename T, typename... S> inline void println(T outVar, S... args) {co
 #define all(x) x.begin(), x.end()
 typedef pair<int, int> pii;
 
+int get_difference(int sum, const vector<int>& p, int bitmask, int n) {
+    int orig = sum;
+    for(int i = 0; i<n; i++) {
+        int bit_index = 1 << i;
+        sum -= ((bitmask & bit_index) != 0) ? p[i] : 0;
+    }
+
+    return abs(sum - (orig - sum));
+}
+
 int32_t main() {
     setup();
 
@@ -40,10 +50,33 @@ int32_t main() {
     int n;
     input(n);
 
+    int sum = 0;
     vector<int> p(n);
-    arrPut(p);
+    For(i, 0, n) {
+        input(p[i]);
+        sum += p[i];
+    }
 
-    // now need to test all groups of size 1 to n/2
+    // now need to test all groups of size 0 to n/2
+    // basically iterate and find all binary numbers that have less than or equal to n/2 1's
+
+    // max number we need to go to is number that starts with n/2 1's
+    int max_number = 0;
+    for(int i = 0; i<n/2; i++) {
+        max_number += (1 << (n - 1 - i));
+    }
+
+    int min_diff = LLONG_MAX;
+    int tested = 0;
+
+    for(uint i = 0; i<=max_number; i++) {
+        if (__builtin_popcountll(i) <= n/2) {
+            tested++;
+            int cur_diff = get_difference(sum, p, i, n);
+            min_diff = min(min_diff, cur_diff);
+        }
+    }
     
+    cout << min_diff << endl;
     return 0;
 }
