612e7184bc6b9e48997b0c09e1dcb17bb2c45820
Jeffrey Gong <jeffrey.gong@yale.edu>
2024-01-08 15:01:00 -0500

    unfinished shit

---
+++ cses/dynamic-programming/minimizing-coins.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<numeric>
+
+using ull = unsigned long long;
+using ll = long long;
+using ld = long double;
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define endl "\n";
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void println(T outVar) {cout << outVar << endl;}
+template<typename T, typename... S> inline void println(T outVar, S... args) {cout << outVar << endl; print(args ...);}
+#define ff first
+#define D(x) cerr << #x " is " << (x) << endl
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << endl
+#define foreach(x, v) for (__typeof__ (v).begin() x=(v).begin(); x != (v).end(); ++x)
+#define For(i, a, b) for (int i=(a); i<(b); ++i)
+#define DFor(i, a, b) for (int i=(a); i>=b; --i)
+#define ss second
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define int ll
+#define uint ull
+#define odd(x) (x & 1)
+#define all(x) x.begin(), x.end()
+typedef pair<int, int> pii;
+
+int32_t main() {
+    setup();
+
+    int n, s;
+    input(n, s);
+
+    vector<int> coins(n);
+    arrPut(coins);
+
+
+    return 0;
+}
